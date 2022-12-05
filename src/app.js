c9a1a6e0aa6006bb162e1d5f60ce600ea18909af
Hosna <blackbutterfllly@gmail.com>
2022-12-05 17:42:37 +0330

    get real-time date from api
    get real time date from api response,
    format the given date,
    replace the html elements with real data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@
 const city = "tehran";
 const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (hours < 10) hours = `0${hours}`;
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = daysOfWeek[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   const temperatureElement = document.getElementById("weather-temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -17,6 +36,9 @@ function displayTemperature(response) {
 
   const windElement = document.getElementById("wind");
   windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+
+  const dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 axios.get(apiUrl).then(displayTemperature);
